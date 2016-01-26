47bd4dd144e1b6dadd15a5352051747e50081cb5
AquaticMatador <nmotley@carthage.edu>
2016-01-26 14:56:07 -0600

    Added Files

---
+++ chat.js
@@ -0,0 +1,22 @@
+$(document).ready(function(){
+	var firebaseRef = new Firebase("https://tommyleejones.firebaseio.com/");
+	var messagesRef = firebaseRef.child("messages");
+
+		messagesRef.on("child_added", function(snapshot, previousNeighborId) {
+		var data = snapshot.val();
+		$("#bottom").before("<div class='container'><div class='col-md-4'><h3>" + data.user + "</div><div class='col-md-8'><h3>" + data.message + "</h3></div></div><br />");
+	});
+		$("#chat").click(function(){
+			var userAuth = firebaseRef.getAuth();
+			console.log(userAuth.uid);
+			var userRef = firebaseRef.child(userAuth.uid);
+			var usersname = userRef.child("/userName");
+			var usersName = userRef.orderByKey().on("child_added", function(snapshot) {
+				data = snapshot.val();
+
+			var userMessage = $("#message").val();
+			messagesRef.push({user: data, message: userMessage});
+			});
+		});
+
+});
---
+++ logon.js
@@ -0,0 +1,63 @@
+$(document).ready(function() {
+	randomAll();
+	var firebaseRef = new Firebase("https://tommyleejones.firebaseio.com/");
+		  $("#username").val("");
+		  $("#password").val("");
+		  $("#usname").val("");
+		  $("#uspass").val("");
+	$("#login").click(function(){
+		var username = $("#name").text();
+		//var userpass = $("#uspass").val();
+		firebaseRef.authAnonymously(function(error, authData) {
+		if (error) {
+			$("#failLog").text("Login Was Not Successfull, please try again");
+		} else {
+			var user = firebaseRef.getAuth();
+			var userRef = firebaseRef.child(user.uid);
+			userRef.set({userName: username});
+		    console.log("Authenticated successfully with payload:", authData);
+		    window.location.assign("index.html");
+		  }
+		}, { rmember: "sessionOnly"});
+		/*
+		firebaseRef.authWithPassword({
+			email: username, password: userpass, remember: "sessionOnly"
+		},  function(error, authData) {
+			  if (error) {
+			    console.log("Login Failed!", error);
+			    $("#failLog").text("Login Was Not Successfull, please try again");
+			  } else {
+			    //console.log("Authenticated successfully with payload:", authData);
+			    window.location.assign("index.html");
+			  }
+			}, {
+				remember: "sessionOnly"
+			}); 
+*/
+	});
+
+	$("#new").click(function(){
+		randomAll();
+	});
+});
+
+//grab any random article title (so you could end up with a coat of arms or some unkown artist's discography)
+            function randomAll() {
+                $.ajax({
+                    type: "GET",
+                    url: "http://en.wikipedia.org/w/api.php?action=query&list=random&format=json&rnnamespace=0&rnlimit=1&callback=?",
+                    contentType: "application/json; charset=utf-8",
+                    async: false,
+                    dataType: "json",
+                    success: function (data, textStatus, jqXHR) {
+                        console.log(data.query.random[0].title);
+                        console.log(data.query.random[0]);
+                        $("#usname").html="";
+                        $("#usname").html("<a href = 'http://en.wikipedia.org/wiki/?curid=" + data.query.random[0].id + "' target='_blank' id='name'>" + data.query.random[0].title + "</a>");
+                        //var htmlstring = "You are <a href = 'http://en.wikipedia.org/wiki/?curid=" + data.query.random[0].id + "'>" +data.query.random[0].title + "</a>";
+                        //$('#article').html(htmlstring);
+                    },
+                    error: function (errorMessage) {
+                    }
+                });
+            }
